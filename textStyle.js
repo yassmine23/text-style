52c78564a9314cede883e9cad4aeb3fa7cf6ca12
yassmin23 <yasminessam827@gmail.com>
2022-05-29 01:12:45 +0200

    initial commit

---
+++ textStyle.js
@@ -0,0 +1,37 @@
+var p= document.getElementById("PAR");
+
+function ChangeFont(font) {
+    p.style.fontFamily=font;
+  }
+
+  function ChangeAlign(align) {
+    p.style.textAlign=align;
+  }
+
+  function ChangeHeight(height) {
+    p.style.lineHeight=height;
+  }
+
+  function ChangeLSpace(space) {
+    p.style.letterSpacing=space;
+  }
+
+  function ChangeIndent(indent) {
+    p.style.textIndent=indent;
+  }
+
+  function ChangeTransform(transform) {
+    p.style.textTransform =transform;
+  }
+
+  function ChangeDecorate(decor) {
+    p.style.textDecoration=decor;
+  }
+
+  function ChangeBorder(border) {
+    p.style.border=border;
+  }
+
+function ChangeBorderColor(border) {
+    p.style.borderColor=border;
+  }
